65bde0fffa20353234ec58ac9c6c5f263dc17100
vviikkii <tizzies-0pouched@icloud.com>
2023-07-11 18:45:24 +0200

    4th commit

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,30 @@
+import connect from "@/utils/db";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export const POST = async (request) => {
+    const {name, email, password} = await request.json();
+
+    await connect();
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    const newUser = new User(
+      {
+        name,
+        email,
+        password: hashedPassword,
+      });
+     try{
+        await newUser.save()
+        return new NextResponse("User successfully created", {
+            status: 201,
+        })
+     
+     }catch(err){
+        return new NextResponse(err.message, {
+            status: 500,
+        });
+     }
+};
