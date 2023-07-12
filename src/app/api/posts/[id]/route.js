607b54339bf881f0d8950c60fd4dbf70dc4dc744
vviikkii <tizzies-0pouched@icloud.com>
2023-07-13 01:12:42 +0200

    6th commit

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -15,5 +15,19 @@ export const GET = async ( request, {params}) => {
     } catch (err) {
         return new NextResponse("Database Error!", {status: 500});
     }
-    
-}
+};
+
+
+export const DELETE = async (request, { params }) => {
+    const { id } = params;
+  
+    try {
+      await connect();
+  
+      await Post.findByIdAndDelete(id);
+  
+      return new NextResponse("Post has been deleted", { status: 200 });
+    } catch (err) {
+      return new NextResponse("Database Error", { status: 500 });
+    }
+  };
